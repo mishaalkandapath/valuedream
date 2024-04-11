d4b0696491938026a5d39d8c6f4c52a6005b8fd0
Devan Srinivasan <devan.srinivasan@mail.utoronto.ca>
2024-04-11 17:30:37 -0400

    rearranged some stuff

--- dreamerv2/agent.py
+++ dreamerv2/agent.py
@@ -248,16 +248,21 @@ class ActorCritic(common.Module):
             critic_loss, mets4 = self.critic_loss(seq, target)
         actor_loss, mets3 = self.actor_loss(seq, target)
     
+    
+    #Update the actor
+    metrics.update(self.actor_opt(actor_tape, actor_loss, self.actor))
+
     #Printing weights for debugging
     tf.print('pre-update')
     model_weights = [var for var in world_model.rssm.trainable_variables]
     tf.print(model_weights[0])
-    metrics.update(self.critic_opt(critic_tape, critic_loss, [self.critic, world_model.rssm])) #Remove critic.loss from array to see weight update on just the world_model.rssm.
+    
+    # Update the critic
+    metrics.update(self.critic_opt(critic_tape, critic_loss, [self.critic, world_model.rssm])) 
+    
     tf.print('post-update')
     model_weights = [var for var in world_model.rssm.trainable_variables]
     tf.print(model_weights[0])
-    #Update the actor
-    metrics.update(self.actor_opt(actor_tape, actor_loss, self.actor))
     
     metrics.update(**mets1, **mets2, **mets3, **mets4)
     self.update_slow_target()  # Variables exist after first forward pass.
