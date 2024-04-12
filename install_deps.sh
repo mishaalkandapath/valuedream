df04fe06a4b7ec20ac9697ac6373f1adc7863c2d
Devan Srinivasan <devan.srinivasan@mail.utoronto.ca>
2024-04-12 23:45:19 +0000

    additions

---
+++ install_deps.sh
@@ -0,0 +1,5 @@
+pip3 install tensorflow==2.15 
+pip3 install tensorflow_probability==0.23
+pip3 install ruamel.yaml
+pip3 install gym==0.26.2
+pip3 install crafter==1.8.3
--- vm_setup.sh
+++ vm_setup.sh
@@ -1,8 +1,4 @@
+git config --global user.email "devan.srinivasan@mail.utoronto.ca"
+git config --global user.name "Devan Srinivasan"
 sudo apt-get install python3-venv
 python3.9 -m venv vnv
-source vnv/bin/activate
-pip3 install tensorflow==2.15 
-pip3 install tensorflow_probability==0.23
-pip3 install ruamel.yaml
-pip3 install gym==0.26.2
-pip3 install crafter==1.8.3
