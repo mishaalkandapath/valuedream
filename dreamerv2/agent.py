d37ffe3834cd6ca7eec0a95f375fd31229f12c11
sopleee <leesophie99@gmail.com>
2024-04-12 14:53:15 -0400

    fix size 2

--- dreamerv2/agent.py
+++ dreamerv2/agent.py
@@ -398,7 +398,7 @@ class ActorCritic(common.Module):
     print(row[47:])
     print(tf.pad(row[47:], tf.constant([[0,hor-(seqlen - 47)]]), "CONSTANT"))
     fat_row = tf.stack([row[i:i+hor] if i <= (seqlen - hor) else tf.pad(row[i:], tf.constant([[0,hor-(seqlen-i)]]), "CONSTANT") 
-               for i in range(hor)])
+               for i in range(seqlen)])
     print("FAT ROW:::",fat_row)
     # output = tf.zeros(expected_post_val.shape, dtype=tf.float32)
     # new_images = tf.concat([tf.zeros() + expected_post_val[i:, :] for i in range(0, 5)], 0)
