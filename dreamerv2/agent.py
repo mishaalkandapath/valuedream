fb2e32ac89ffd1a20012a8ab2e9b87ed59e9a4f6
sopleee <leesophie99@gmail.com>
2024-04-12 20:55:37 -0400

    bool

--- dreamerv2/agent.py
+++ dreamerv2/agent.py
@@ -384,7 +384,7 @@ class ActorCritic(common.Module):
       start += hor*(obslen-hor+1)
       extra_seq = [flat_seq[start+(j*hor):start+(j*hor)+(hor-j)] for j in range(0,hor)]
       
-      if not accum_seq: accum_seq = tf.concat([batch_accum, tf.concat(extra_seq,0)],0)
+      if accum_seq is None: accum_seq = tf.concat([batch_accum, tf.concat(extra_seq,0)],0)
       else: tf.concat([accum_seq, batch_accum, tf.concat(extra_seq,0)],0)
         
     # shape = (obslen*n_batches*hor - the unneeded parts, 2048)
